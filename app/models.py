8098eba35b70c173510832dab8941e5339a0fe09
Guiiilhermee rodrigues <guilherme_rodrigues91@hotmail.com>
2024-02-04 23:57:14 +0000

    Changed note field to service in all CRUD and made migrations

--- app/models.py
+++ app/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 
+class Service(models.Model):
+    title = models.CharField(max_length=50)
+
 class Record(models.Model):
 
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=255)
     phone = models.CharField(max_length=20)
-    date = models.DateField(blank=True,null=True)
-    time = models.CharField(max_length=10,blank=True,null=True)
-    note = models.CharField(max_length=100,blank=True,null=True)
+    date = models.DateTimeField(blank=True, null=True)
+    time = models.CharField(max_length=10,blank=True, null=True)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE, max_length=100, blank=True, null=True)
 
 
     def __str__(self):
