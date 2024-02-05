6b0364010a9b05057581462ad8904d51fd64a898
Guiiilhermee rodrigues <guilherme_rodrigues91@hotmail.com>
2024-02-05 00:16:25 +0000

    Changed date time field

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ class Record(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=255)
     phone = models.CharField(max_length=20)
-    date = models.DateTimeField(blank=True, null=True)
+    date = models.DateField(blank=True, null=True)
     time = models.CharField(max_length=10,blank=True, null=True)
     service = models.ForeignKey(Service, on_delete=models.CASCADE, max_length=100, blank=True, null=True)
 
