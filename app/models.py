413c099b942371b540ccc2819d4deedcbf69a5a1
Guiiilhermee rodrigues <guilherme_rodrigues91@hotmail.com>
2024-02-05 02:38:15 +0000

    Changed notes to Service and prices on SQL

--- app/models.py
+++ app/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 class Service(models.Model):
     title = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.title
 
 class Record(models.Model):
 
