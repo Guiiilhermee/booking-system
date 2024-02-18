1ff97ac9d6165817c5b723a6c8e17db2288315b7
Guiiilhermee rodrigues <guilherme_rodrigues91@hotmail.com>
2024-02-18 22:44:14 +0000

    Add Allowed hosts

--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-barber-cef3846359e0.herokuapp.com']
 
 
 # Application definition
